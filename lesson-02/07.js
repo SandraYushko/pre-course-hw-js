d419493e22ac5a8f0d26a313fb7dcfd1fc982f15
SandraYushko <shyria6@gmail.com>
2024-01-30 23:33:52 +0300

    lesson02 DZ #07 (4)

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -8,7 +8,7 @@ let passportMarried = {
 };
 
 let passportMarried2 = { ...passportMarried, married: true, address: { ...passportMarried.address } }
-passportMarried.married = false
+passportMarried2.married = true
 
 console.log(passportMarried);
 console.log(passportMarried2);
